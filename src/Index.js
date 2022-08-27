9d0da86f00d7ec6089843b9fc3dccf0dc19f73c7
SaraChaLo <112277449+SaraChaLo@users.noreply.github.com>
2022-08-27 20:43:35 +0100

    Project uploaded

---
+++ src/Index.js
@@ -0,0 +1,84 @@
+let now = new Date();
+function getDay() {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return day;
+}
+function getTime() {
+  var hour = now.getHours();
+  var minutes = now.getMinutes();
+  var temp = (minutes < 10 ? "0" : "") + minutes;
+  return `${hour}:${temp}`;
+}
+let date = document.querySelector("#date");
+date.innerHTML = `${getDay()} ${getTime()}`;
+
+function citySearch(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector(`#searchCityInput`);
+
+  let units = "metric";
+  let apiKey = "f2301ba492968c3302dbd963926e40f9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+function showWeather(weather) {
+  console.log(weather);
+  let temperatureValue = Math.round(weather.data.main.temp);
+
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = temperatureValue;
+
+  showCity(weather.data.name);
+}
+function showCity(cityValue) {
+  let city = document.querySelector("#city");
+  city.innerHTML = cityValue;
+}
+
+let formSearchCity = document.querySelector("#search-city");
+formSearchCity.addEventListener("submit", citySearch);
+
+function currentCityWeather(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "92030bd4fbf5b53c36e529ff0bce0abb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+let currentButton = document.querySelector("#current");
+currentButton.addEventListener("click", currentCityWeather);
+
+//cambiar grados
+
+function toCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = 19;
+}
+let linkCelsius = document.querySelector("#celsius");
+linkCelsius.addEventListener("click", toCelsius);
+
+function toFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = 66;
+}
+let linkFahrenheit = document.querySelector("#fahrenheit");
+linkFahrenheit.addEventListener("click", toFahrenheit);
